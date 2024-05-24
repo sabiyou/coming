6b76da3032015241a2c066a00e7820ee19d0d0af
sabiyou <luciusemuh@gmail.com>
2024-05-24 17:22:29 +0100

    last commit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
-pip install django
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
+echo "BUILD START" 
+python3.9 -m ensurepip
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt 
+python3.9 manage.py collectstatic --noinput --clear 
+echo "BUILD END"
